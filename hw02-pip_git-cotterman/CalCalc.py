595bdde623a0742b58595ca1bc0cf08e6f6fb3ab
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-15 15:43:18 -0700

    adding first draft of CalCalc.py

---
+++ hw02-pip_git-cotterman/CalCalc.py
@@ -0,0 +1,23 @@
+# Instructions: Write a modeul called CalCalc, with a method called "calculate" that evaluates any string passed to it,
+# and can be used from either the command line (using argparse with reasonable flags) or imported within Python
+# Feel free to use something like eval(), but be aware of some of the nasty things it can do, and make sure it doesn't have too much power:
+# http://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html
+
+
+import argparse
+parser = argparse.ArgumentParser(description='User input to parse')
+parser.add_argument('mystring', help='This is the string to evaluate')
+parser.add_argument('-t', action='store_true', default=True,
+                    dest='boolean_switch',
+                    help='Set a switch to true')
+parser.add_argument('-f', action='store_false', default=True,
+                    dest='boolean_switch',
+                    help='Set a switch to false')
+
+results = parser.parse_args()
+evaluation = eval(results.mystring)
+
+
+print 'The expression "' , results.mystring, '" evaluates to: \n', evaluation
+print 'Evaluated using the Wolfram-Alpha API   =', results.boolean_switch
+
