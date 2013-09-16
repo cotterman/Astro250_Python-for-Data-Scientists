fb1c94216c71d31f40475745c4d66d0adb89f224
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-15 20:36:36 -0700

    now all files should be added properly.  (adding the new folder appears to have added everything inside of folder)

---
+++ hw02-pip_git-cotterman/setup.py
@@ -0,0 +1,11 @@
+
+
+import urllib2
+import urllib
+from distutils2.core import setup
+
+setup(name='CalCalc_package',
+    version='0.01',
+     license='LICENSE.txt',
+    py_modules=['CalCalc_package'],
+    )
