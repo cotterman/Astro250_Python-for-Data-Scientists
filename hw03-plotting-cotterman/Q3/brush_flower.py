24ff33929b5436dc779926a9047f79034ba85aff
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-25 00:49:46 -0700

    brush_flower.py now produces a decent-looking q3_flowers_stationary.pdf (I fixed the size and titles.)  Now I need to figure out how to interact with it.

--- hw03-plotting-cotterman/Q3/brush_flower.py
+++ hw03-plotting-cotterman/Q3/brush_flower.py
@@ -6,10 +6,6 @@ import pandas as pd
 # Read in data
 
 q3data = pd.read_csv('flowers.csv')
-
-
-# Plot the iris dataset in a 4x4 grid
-
 traits = list(q3data.columns)[:4]
 unique_species = set(q3data['species'])
 #print unique_species
@@ -17,13 +13,21 @@ unique_species = set(q3data['species'])
 ctable = {'setosa':'r','versicolor':'g','virginica':'b'}
 ctable['versicolor']
 
+
+# Plot the data in a 4x4 grid
+
 f, axes = plt.subplots(nrows=4, ncols=4)
+f.set_size_inches(18., 12.)
 
 for row, trait1 in enumerate(traits):
     for col, trait2 in enumerate(traits):
         axes[row,col].scatter(q3data[trait1], q3data[trait2], c=[ctable[s] for s in q3data['species']])
-
-axes[0,0].set_title('Flower Characteristics')
+        if col==row:
+            axes[row,col].set_title(trait1, position=(.1, .9), 
+                                    horizontalalignment='left', verticalalignment='top')
+        
+f.suptitle('Flower Characteristics', fontsize='x-large')
+print f.get_size_inches() #default is [6. 4.]
 
 #save plot
 plt.savefig('q3_flowers_stationary.pdf')
