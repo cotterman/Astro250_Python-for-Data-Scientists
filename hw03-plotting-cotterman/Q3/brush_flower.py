51b9ff176547b6678d5a32f63d8f0d186e496604
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-25 00:11:37 -0700

    brush_flower.py will create grid of 4x4 plots, but with no 'brushing' and ugly formating

--- hw03-plotting-cotterman/Q3/brush_flower.py
+++ hw03-plotting-cotterman/Q3/brush_flower.py
@@ -1,16 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from mpl_toolkits.basemap import Basemap 
 
-# Data descriptor to make a proper array.
-dt1 = [('Date', np.int), ('Value', np.float)]
-dt2 = [('Date', np.int), ('Temperature', np.int)]
-dyahoo = np.loadtxt('yahoo_data.txt', skiprows=1, dtype=dt1)
+
+# Read in data
+
+q3data = pd.read_csv('flowers.csv')
 
 
 # Plot the iris dataset in a 4x4 grid
-f, ax = plt.subplots()
+
+traits = list(q3data.columns)[:4]
+unique_species = set(q3data['species'])
+#print unique_species
+#print len(traits)
+ctable = {'setosa':'r','versicolor':'g','virginica':'b'}
+ctable['versicolor']
+
+f, axes = plt.subplots(nrows=4, ncols=4)
+
+for row, trait1 in enumerate(traits):
+    for col, trait2 in enumerate(traits):
+        axes[row,col].scatter(q3data[trait1], q3data[trait2], c=[ctable[s] for s in q3data['species']])
+
+axes[0,0].set_title('Flower Characteristics')
+
+#save plot
+plt.savefig('q3_flowers_stationary.pdf')
+
+
 
 # Interactively draw rectangles on one of the subplots
 
