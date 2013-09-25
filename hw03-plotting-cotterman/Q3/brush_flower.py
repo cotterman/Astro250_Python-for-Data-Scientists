9fa40b5e4ba509bbd07bada12a4f265582f3e832
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-25 14:40:59 -0700

    Changed code so it now uses classes.  Plot still not interactive

--- hw03-plotting-cotterman/Q3/brush_flower.py
+++ hw03-plotting-cotterman/Q3/brush_flower.py
@@ -2,40 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+class FlowerPlot(object):
 
-# Read in data
+    """Create a plot that displays traits in grid of scatter plots"""
 
-q3data = pd.read_csv('flowers.csv')
-traits = list(q3data.columns)[:4]
-unique_species = set(q3data['species'])
-#print unique_species
-#print len(traits)
-ctable = {'setosa':'r','versicolor':'g','virginica':'b'}
-ctable['versicolor']
+    # we use a leading _ to indicate that the method is private (
+    # ie, should only be called from within the object )
+    def _init_plot(self):
+        self.f, self.axes = plt.subplots(nrows=4, ncols=4)
+        self.f.set_size_inches(18., 12.)
+        self.f.suptitle('Flower Characteristics', fontsize='x-large')
 
+        ctable = {'setosa':'r','versicolor':'g','virginica':'b'}
+        for row, trait1 in enumerate(self.traits):
+            for col, trait2 in enumerate(self.traits):
+                colors = [ctable[s] for s in self.q3data['species']]
+                self.axes[row,col].scatter(
+                    self.q3data[trait1], self.q3data[trait2], c=colors )
+                
+                if col==row:
+                    self.axes[row,col].set_title(
+                        trait1, position=(.1, .9), 
+                        horizontalalignment='left', verticalalignment='top')
+                
+        return
+
+    # __init__ is called whenever an object instance is initialized
+    def __init__(self, data_fname):
+        self.q3data = pd.read_csv(data_fname)
+        self.traits = list(self.q3data.columns)[:4]
+        self.unique_species = set(self.q3data['species'])
+
+        # mapping from flower type to color
+        self._init_plot()
+        return
 
 # Plot the data in a 4x4 grid
+my_plot = FlowerPlot('flowers.csv')
 
-f, axes = plt.subplots(nrows=4, ncols=4)
-f.set_size_inches(18., 12.)
-
-for row, trait1 in enumerate(traits):
-    for col, trait2 in enumerate(traits):
-        axes[row,col].scatter(q3data[trait1], q3data[trait2], c=[ctable[s] for s in q3data['species']])
-        if col==row:
-            axes[row,col].set_title(trait1, position=(.1, .9), 
-                                    horizontalalignment='left', verticalalignment='top')
-        
-f.suptitle('Flower Characteristics', fontsize='x-large')
-print f.get_size_inches() #default is [6. 4.]
-
-#save plot
-plt.savefig('q3_flowers_stationary.pdf')
-
-
+# plt.savefig('q3_flowers_stationary.pdf')
 
 # Interactively draw rectangles on one of the subplots
 
 # Identify the datapoints located within the drawn rectangle
 
 # Change the color/opacities of the corresponding points in the other subplots 
+
+
