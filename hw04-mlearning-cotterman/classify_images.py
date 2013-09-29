523e785169375c7acfa62777efb555fca10b8198
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-28 20:58:17 -0700

    Will now compute 6 basic features for an image.

--- hw04-mlearning-cotterman/classify_images.py
+++ hw04-mlearning-cotterman/classify_images.py
@@ -2,9 +2,13 @@
 import numpy as np
 import scipy as sp
 import os  #The os.path module has several functions for manipulating files and directories
+import matplotlib.pyplot as plt
 from scipy import ndimage
 from scipy import misc
+from scipy import stats
 from scipy.ndimage import imread
+import scipy.ndimage as ndi
+
 
 #Assume this script is in same folder as the "50_categories" folder
     #and assume the "50_categories" folder contains images
@@ -19,11 +23,13 @@ categories = os.listdir(os.path.join(ppath, foldername))
 print categories
 
 #obtain images within each category
-for category in categories:
-    datapath = os.path.join(ppath, foldername, category)
-    images_in_cat = os.listdir(datapath) #list of all image names in category
-    for image_name in images_in_cat:
-        image = misc.imread(datapath + "/" + image_name) 
+#for category in categories:
+ #   if category != ".DS_Store": #would be cleaner to take this out of the "categories" list
+  #      datapath = os.path.join(ppath, foldername, category)
+   #     images_in_cat = os.listdir(datapath) #list of all image names in category
+    #    for image_name in images_in_cat:
+            #print image_name
+            #image = misc.imread(datapath + "/" + image_name) 
     
 # Write a set of methods that takes as input one of these images, and then computes real-numbered
 #features as the return.  You should produce at least 15 features. 
@@ -31,7 +37,21 @@ category = "camel"
 datapath = os.path.join(ppath, foldername, category)
 image_name = str(category) + "_0001" + ".jpg"
 image = misc.imread(datapath + "/" + image_name)
+plt.imshow(image)
 print type(image)
+
+#Average of each of the channels (R, G, B)
+meanR = image[:,:,0].mean()
+meanG = image[:,:,1].mean()
+meanB = image[:,:,2].mean()
+
+#Standard deviation of each of the channels (R, G, B)
+stdR = image[:,:,0].std()
+stdG = image[:,:,1].std()
+stdB = image[:,:,2].std()
+
+features = [meanR, meanG, meanB, stdR, stdG, stdB]
+print features
 
 # Based on the feature set for each image, build a random forest classifier (scikits.learn).
 #Produce metrics on your estimated error rates using cross-validation. How much better is this
