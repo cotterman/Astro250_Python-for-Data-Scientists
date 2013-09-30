995c79c25558957e25cc88915691659074510a0a
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-30 16:04:29 -0700

    grid search finds best random forest classifier.  I use it to get optimal model, and also to find 3 most important features.

--- hw04-mlearning-cotterman/classify_images.py
+++ hw04-mlearning-cotterman/classify_images.py
@@ -11,12 +11,14 @@ import scipy.ndimage as ndi
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import cross_validation
 from sklearn.cross_validation import cross_val_score
+from sklearn import grid_search
+from sklearn import metrics
 
 def get_ppath():
     """
     Assume this script is in same folder as the "50_categories" folder
     and assume the "50_categories" folder contains images
-    in folders labeled according to category (e.g., "camel", "airplane", etc.)
+    in folders labeled according to category (e.g., "camel", "airplane", etc.).
     """
     print ("This directory should contain script and folder of folders with images: ")
     ppath , file = os.path.split(os.path.realpath(__file__))
@@ -25,13 +27,16 @@ def get_ppath():
 
 def get_categories(ppath, foldername):
     """
-    Obtain list of categories, taken from folder titles
+    Obtain list of categories, taken from folder titles.
     """
     categories = os.listdir(os.path.join(ppath, foldername)) 
     categories[:] = [c for c in categories if c != ".DS_Store"]
     return categories
 
 def obtain_features(image):
+    """
+    Calculate image features, to be used as input for model.
+    """
     
     #for color images
     if image.ndim>2:
@@ -56,77 +61,123 @@ def obtain_features(image):
     return features
 
 def summarize_images(categories, ppath, foldername):
+    """
+    For each image, obtain image category and features list.
+    """
     images_data = [] #create a list to contain summarized image data
     for category in categories:
         datapath = os.path.join(ppath, foldername, category)
         images_in_cat = os.listdir(datapath) #list of all image names in category
         for image_name in images_in_cat:
             image = misc.imread(datapath + "/" + image_name)
-            #print image_name
             image_features = obtain_features(image)
             image_features.append(category)
             images_data.append(image_features)
     return images_data
 
 def examine_images_data(images_data):
+    """
+    Examine content and type of data stored in images_data.
+    """
     print "type(images_data): ", type(images_data) #we have a list
     print "number of images: " , len(images_data) #4244 images
-    print "#features: " , len(images_data[15]) - 1 #each image has Xx features + category name
-    print "example of feature values: " , images_data[15] #list containing features and category for image 15
+    print "#features: " , len(images_data[0]) - 1 #each image has Xx features + category name
+    print "example of feature values: " , images_data[0] #list containing features and category for image 15
+
+
+def random_guessing(Y, categories):
+    """
+    Calculate expected value of loss function if we guessed randomly.
+    """
+    print "Guess each category with equal probability: "
+    print "    Expected score (fraction of guesses that are correct): " , (float(len(Y))/len(categories))/len(Y)
+
+
+def feature_combos(images_data):
+    nfeatures = len(images_data[0])-1
+    feature_choices = [] #create a list of lists
+    for i in range(nfeatures):
+        f1 = i
+        for j in range(i+1, nfeatures):
+            f2 = j
+            for k in range(j+1, nfeatures): 
+                f3 = k
+                feature_choices.append([f1, f2, f3])
+    return feature_choices
+
+
+def best_random_forest(X, Y, nfolds, parameters):
+    """
+    Fit "best" random forest by using parameters that minimize loss function 
+    (i.e., maximize the score)
+    """
+    rf_tune = grid_search.GridSearchCV(RandomForestClassifier(), parameters,\
+                                       score_func=metrics.accuracy_score, n_jobs = 2, cv = nfolds)
+    return rf_tune.fit(X, Y)
+
+def print_results(rf_opt):
+    print("Grid of scores:\n" + str(rf_opt.grid_scores_) + "\n")
+    print("Best zero-one score: " + str(rf_opt.best_score_) + "\n")
+    print("Optimal Model:\n" + str(rf_opt.best_estimator_) + "\n")
+
+
+def best_feature_combo(feature_choices, X, Y, nfolds2, parameters2):
+    """
+    Find combination of features that produces highest score.
+    """
+    best_combo = [0,0,0]
+    best_score = 0
+    for fcombo in feature_choices:
+        findices = np.array(fcombo)
+        X2 = np.array([f[findices] for f in X]) #just the chosen features
+        rf_opt = best_random_forest(X2, Y, nfolds2, parameters2)
+        score = rf_opt.best_score_
+        if score > best_score:
+            best_score = score
+            best_combo = fcombo
+    print "Indices of most important 3 features: \n" , best_combo
+    print "Score obtained using just these features: \n" , best_score
 
 
 ###############################################################################
 
 def main():
     
+    #obtain image information (e.g., calculate features)
+
     ppath = get_ppath() #path of folder containing images
     foldername = "50_categories" #name of folder containing images
     categories = get_categories(ppath, foldername) #list of image categories
     images_data = summarize_images(categories, ppath, foldername) #list of lists
     examine_images_data(images_data) #print basic info on what images_data contains
-
-    
-    #get inputs for random forest
-    # training
-    #train = 500
-    #Xtr = digits['data'][:train]
-    #Ytr = digits['target'][:train]
-    #print("training size: " + str(len(Ytr)))
-    # testing set
-    #Xte = digits['data'][train:]
-    #Yte = digits['target'][train:]
-    #print("testing size: " + str(len(Yte)))
-
-    #build classifier using 9/10 of data
-    ntree = 10 #number of trees in the forest
-    mtry = "sqrt" #number of features to consider when looking for best split
-    rf1 = RandomForestClassifier(n_estimators=ntree, max_features=mtry)
-     #   print rf1.feature_importances
-    #rf1.fit(Xtr, Ytr)
-    #test classifier using remaining 1/10
-      #  preds = rf1.predict(Xte)
-      #  print float(sum(preds!=Yte))/len(Yte)
-
-    #Use cross-validation function
     X = np.array([f[:-1] for f in images_data]) #just the features (X will be a list of lists)
     Y = np.array([c[-1] for c in images_data]) #list of categories
-    nfolds = 3 
 
-    def print_cv_score_summary(model, xx, yy, cv):
-        scores = cross_val_score(model, xx, yy, cv=cv, n_jobs=-1)
-        print("mean: {:3f}, stdev: {:3f}".format(np.mean(scores), np.std(scores)))
+    random_guessing(Y, categories) #Expected score if guessing category randomly 
 
-    print_cv_score_summary(rf1, X, Y,
-        cv=cross_validation.KFold(len(Y), nfolds, shuffle=True, random_state=1))
+
+    #build classifier using "best" random forest
+
+    nfolds = 3 #number of folds to use for cross-validation
+    parameters = {'n_estimators':[50,100],  'max_features':[5,6]} # rf parameters to try
+        #n_estimators is number of trees in forest
+        #max_features is the number of features to consider when looking for best split
+    rf_opt = best_random_forest(X, Y, nfolds, parameters)
+    print_results(rf_opt) #Results of the grid search for optimal random forest parameters.
+
+
+    #What are the 3 most important features? Test 455 combos (455 = 15 choose 3).
+  
+    feature_choices = feature_combos(images_data) #list of 3-feature options
+    nfolds2 = 3
+    parameters2 = {'n_estimators':[50,100]} # rf parameters to try
+    best_feature_combo(feature_choices, X, Y, nfolds2, parameters2)
     
 main()
  
 
 ###############################################################################
 
-# Based on the feature set for each image, build a random forest classifier (scikits.learn).
-#Produce metrics on your estimated error rates using cross-validation. How much better is this
-#than the expectation with random guessing? What are the 3 most important features?
 
 
 # Make sure your final classifier can run on a directory of different images, where a call like:
