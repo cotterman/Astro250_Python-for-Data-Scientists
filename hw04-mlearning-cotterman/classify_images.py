5ba793ae1cb9c57f288869587b4a4561b7711936
Carolyn Cotterman <Cotterman@gmail.com>
2013-09-30 17:16:04 -0700

    now classifies images from validation_images folder.

--- hw04-mlearning-cotterman/classify_images.py
+++ hw04-mlearning-cotterman/classify_images.py
@@ -13,6 +13,8 @@ from sklearn import cross_validation
 from sklearn.cross_validation import cross_val_score
 from sklearn import grid_search
 from sklearn import metrics
+import pickle
+
 
 def get_ppath():
     """
@@ -60,9 +62,9 @@ def obtain_features(image):
     features = [meanR, meanG, meanB, stdR, stdG, stdB]
     return features
 
-def summarize_images(categories, ppath, foldername):
+def summarize_trimages(categories, ppath, foldername):
     """
-    For each image, obtain image category and features list.
+    For each image in training data, obtain image category and features list.
     """
     images_data = [] #create a list to contain summarized image data
     for category in categories:
@@ -74,6 +76,21 @@ def summarize_images(categories, ppath, foldername):
             image_features.append(category)
             images_data.append(image_features)
     return images_data
+
+def summarize_vimages(ppath, foldername):
+    """
+    for each image in validation set, obtain feature list.
+    """
+    images_data = []
+    datapath = os.path.join(ppath, foldername)
+    validation_images = os.listdir(datapath) #list of validation image names
+    for image_name in validation_images:
+        image = misc.imread(datapath + "/" + image_name)
+        image_features = obtain_features(image)
+        image_features.append(image_name)
+        images_data.append(image_features)
+    return images_data
+        
 
 def examine_images_data(images_data):
     """
@@ -143,12 +160,12 @@ def best_feature_combo(feature_choices, X, Y, nfolds2, parameters2):
 
 def main():
     
-    #obtain image information (e.g., calculate features)
+    ### obtain image information (e.g., calculate features) ###
 
     ppath = get_ppath() #path of folder containing images
-    foldername = "50_categories" #name of folder containing images
+    foldername = "50_categories" #name of folder containing training images
     categories = get_categories(ppath, foldername) #list of image categories
-    images_data = summarize_images(categories, ppath, foldername) #list of lists
+    images_data = summarize_trimages(categories, ppath, foldername) #list of lists
     examine_images_data(images_data) #print basic info on what images_data contains
     X = np.array([f[:-1] for f in images_data]) #just the features (X will be a list of lists)
     Y = np.array([c[-1] for c in images_data]) #list of categories
@@ -156,22 +173,37 @@ def main():
     random_guessing(Y, categories) #Expected score if guessing category randomly 
 
 
-    #build classifier using "best" random forest
+    ### build classifier using "best" random forest ###
 
     nfolds = 3 #number of folds to use for cross-validation
-    parameters = {'n_estimators':[50,100],  'max_features':[5,6]} # rf parameters to try
+    parameters = {'n_estimators':[5],  'max_features':[5]} # rf parameters to try
         #n_estimators is number of trees in forest
         #max_features is the number of features to consider when looking for best split
     rf_opt = best_random_forest(X, Y, nfolds, parameters)
     print_results(rf_opt) #Results of the grid search for optimal random forest parameters.
+    RFclassifier = pickle.dumps(rf_opt.best_estimator_) #Save classifier in file "RFclassifier"
 
 
-    #What are the 3 most important features? Test 455 combos (455 = 15 choose 3).
+    ### What are the 3 most important features? Test 455 combos (455 = 15 choose 3) ###
   
     feature_choices = feature_combos(images_data) #list of 3-feature options
     nfolds2 = 3
-    parameters2 = {'n_estimators':[50,100]} # rf parameters to try
-    best_feature_combo(feature_choices, X, Y, nfolds2, parameters2)
+    parameters2 = {'n_estimators':[5]} # rf parameters to try
+    #best_feature_combo(feature_choices, X, Y, nfolds2, parameters2)
+
+
+    ### Use the pickled classifier to classify validation images ###
+    vfolder = "validation_images" #name of folder containing images to classify
+    vimages_data = summarize_vimages(ppath, vfolder) #narray of images features
+    vX = np.array([f[:-1] for f in vimages_data]) #just the features (X will be a list of lists)
+    vNames = np.array([c[-1] for c in vimages_data]) #list of image names
+    clf = pickle.loads(RFclassifier)
+    vY_predicted = np.array(clf.predict(vX))
+    print "\n filename                   predicted_class            "
+    print " ------------------------------------------------------"
+    for i in range(len(vX)):
+        print vNames[i] , "                   " , vY_predicted[i]
+    print "\n"
     
 main()
  
