501fb128a04324afa11dfe2edc61b1c7bc6c1d17
Carolyn Cotterman <Cotterman@gmail.com>
2013-10-16 18:34:34 -0700

    Will grab all historical data.

--- hw06-SQL_webcrawl-cotterman/predict_weather.py
+++ hw06-SQL_webcrawl-cotterman/predict_weather.py
@@ -7,6 +7,7 @@
 
 import sqlite3
 import urllib2
+import os
 import pandas as pd
 
 import numpy
@@ -30,7 +31,6 @@ def get_airport_info(airport_info, test):
             #"local_code","home_link","wikipedia_link","keywords"
     #read_csv won't get tripped up by extra #s and commas like loadtxt does
     airinfo_df = pd.read_csv(airport_info) 
-    connection = sqlite3.connect("weather.db") #in SQLite, this opens up the database
     cursor = connection.cursor() #begin my transaction
 
     #not allowed to make a table that already exists.
@@ -84,7 +84,6 @@ def get_top_airports(airport_list, test):
     #airport_list is a csv that contain a list of the 50 most travelled airports in US
         #Fields: City,FAA,IATA,ICAO,Airport,Role,Enplanements
     topair_data = loadtxt(airport_list, skiprows=1, delimiter=",", dtype=str)
-    connection = sqlite3.connect("weather.db")
     cursor = connection.cursor()
 
     sql_cmd = """
@@ -93,10 +92,13 @@ def get_top_airports(airport_list, test):
     try: cursor.execute(sql_cmd)
     except Exception: pass
 
+    top_airports = [] #list to contain IATA codes of top 50 airports
+
     sql_cmd = """CREATE TABLE topair (id INTEGER PRIMARY KEY AUTOINCREMENT,
         city TEXT, IATA TEXT, airport_name TEXT)"""
     cursor.execute(sql_cmd)
     for airport in topair_data:
+        top_airports.append(airport[2])
         airport_city = airport[0]
         airport_code = airport[2]
         airport_name = airport[4]
@@ -112,6 +114,8 @@ def get_top_airports(airport_list, test):
         db_info = array(cursor.fetchall())
         print db_info
 
+    return top_airports
+
 
 def get_top_airport_info(test):
     """
@@ -119,7 +123,6 @@ def get_top_airport_info(test):
         Match using IATA code
     """
 
-    connection = sqlite3.connect("weather.db")
     cursor = connection.cursor()
 
     #view is a reference to the query that I run which is not stored on disk
@@ -149,11 +152,13 @@ def get_airports(airport_info, airport_list):
     """
 
     ## Read in provided csv files into database
-    get_airport_info(airport_info, test=True) 
-    get_top_airports(airport_list, test=True)
+    get_airport_info(airport_info, test=False) 
+    top_airports = get_top_airports(airport_list, test=False)
 
     #join the tables together to create table view of intersection
-    get_top_airport_info(test=True)
+    get_top_airport_info(test=False)
+
+    return top_airports
 
 
 def build_weather_table(test):
@@ -163,7 +168,6 @@ def build_weather_table(test):
     precipitation, and cloud cover
     """
 
-    connection = sqlite3.connect("weather.db")
     cursor = connection.cursor()
 
     sql_cmd = """
@@ -176,9 +180,13 @@ def build_weather_table(test):
     CREATE TABLE weatherD (
         iata_code TEXT,
         day_num INT,
-        celcius_min FLOAT, 
-        celcius_max FLOAT, 
-        humidity FLOAT, 
+        date TEXT,
+        tempF_max FLOAT,
+        tempF_mean FLOAT,
+        tempF_min FLOAT,
+        humidity_mean FLOAT,
+        wind_mean FLOAT,
+        wind_direction FLOAT,
         precipitation FLOAT,
         cloud_cover INT);
     """
@@ -196,46 +204,206 @@ def build_weather_table(test):
             cursor = connection.cursor()
             cursor.execute(sql_cmd)
             db_info = array(cursor.fetchall())
-            print db_info
+            print "Tables in weather.db: " , db_info
     
     cursor.close() #this seems to not have a purpose (??)
     connection.commit()
-    
 
-def grab_weather_data():
+    
+def download_weather(airport, start_yr, end_yr):
+    """
+    Access a months worth of tabular weather data in csv format from Weather
+        Underground using the ICAO airport code. For instance, 
+        to get the weather for San Francisco airport (ICAO code KSFO) 
+        in the month of September 2013, go to:
+        http://www.wunderground.com/history/airport/KSFO/2013/9/1/MonthlyHistory.html?format=1
+        Write data into csv files.
+    """
+
+    subfolder = "/hw_6_data/MyDownloadedData/" # location of file to be written
+    #ppath , file = os.path.split(os.path.realpath(__file__))
+    ppath = os.getcwd()
+
+    #create subfolder for airport if subfolder does not already exist
+    if os.path.exists(ppath+subfolder+airport) == False:
+        os.mkdir(ppath+subfolder+airport)
+
+    years = range(start_yr, end_yr+1)
+    for year in years:
+        
+        if year<2013: months = 12
+        else: months = 9 #only go thru sept of 2013 since data does not exist for all of October yet
+        for month in range(1, months+1):
+        
+            myfile = str(year) + "_" + str(month) + ".csv" # file to be written
+            file = ppath + subfolder + airport + "/" + myfile
+             
+            url_beg = "http://www.wunderground.com/history/airport/"
+            url_mid = airport + "/" + str(year) + "/" + str(month)
+            url_end = "/1/MonthlyHistory.html?format=1"
+            response = urllib2.urlopen(url_beg + url_mid + url_end)
+            
+            with open(file, 'w') as f: f.write(response.read()) #write to file
+            #the line above is mostly equivalent (but supeior) to running the following 3:    
+                #fh = open(file, "w") #open the file for writing
+                #fh.write(response.read()) # read from request while writing to file
+                #fh.close()
+
+
+def insert_line(line, myday, airport, year):
+
+    dlist = line.split(",")
+    #skip blank first line and line with var names
+    if dlist[0][:4] != str(year):
+        return
+        
+    iata_code = airport
+    day_num = myday
+    
+    try:
+        date = dlist[0]
+        tempF_max = float(dlist[1])
+        tempF_mean = float(dlist[2])
+        tempF_min = float(dlist[3])
+        humidity_mean = float(dlist[8])
+        wind_mean = float(dlist[17])
+        wind_direction = float(dlist[22].split("<")[0]) #don't want the XML tag <br />\\n
+        precipitation = 0 if dlist[19] == 'T' else float(dlist[19])
+        cloud_cover = int(dlist[20])
+    except Exception, inst:
+        print "Could not convert dlist"
+        print inst
+        return
+    
+    data = (iata_code, day_num, date, tempF_min, tempF_max, tempF_mean, 
+            humidity_mean, wind_mean, wind_direction, precipitation, cloud_cover)
+    #print data #data types look as desired
+
+    cursor = connection.cursor()
+    sql_cmd = """
+        INSERT INTO weatherD (
+            iata_code, day_num, date, 
+            tempF_min, tempF_max, tempF_mean,
+            humidity_mean, wind_mean, wind_direction,
+            precipitation, cloud_cover
+        ) VALUES ('%s', %i, '%s', %e, %e, %e,
+            %e, %e, %e, %e, %i
+        )""" % data
+    cursor.execute(sql_cmd)
+    connection.commit()
+
+
+def check_weatherD():
+    """
+    Print some info on what the weatherD table contains.
+    """
+
+    cursor = connection.cursor()
+
+    sql_cmd = """
+        SELECT COUNT(*) FROM weatherD;
+        """
+    cursor.execute(sql_cmd)
+    db_info = array(cursor.fetchall())
+    print "Number of records in weatherD: " , db_info
+
+    sql_cmd = """
+        SELECT DISTINCT iata_code FROM weatherD;
+        """
+    cursor.execute(sql_cmd)
+    db_info = array(cursor.fetchall())
+    print "Airports included in weatherD: " , db_info
+
+    sql_cmd = """
+        SELECT * FROM weatherD WHERE day_num<4;
+        """
+    cursor.execute(sql_cmd)
+    db_info = array(cursor.fetchall())
+    print "First 3 records for each airport in weatherD: " , db_info
+
+
+
+
+def put_weather_in_table(airport, start_yr, end_yr):
+    """
+    Put weather info from CSV files into table in database
+    """
+
+    subfolder = "/hw_6_data/MyDownloadedData/" # location of file to be written
+    ppath = os.getcwd()
+   
+    myday = 0
+    years = range(2012, 2014)
+    for year in years:
+        if year < 2013: months = 12
+        else: months = 9 #only go thru sept of 2013 since data does not exist for all of October yet
+        
+        for month in range(1, months+1):
+            myfile = str(year) + "_" + str(month) + ".csv" # file to be written
+            file = ppath + subfolder + airport + "/" + myfile
+            monthD = open(file, "r") #monthD is a pointer to the file
+            
+            for line in monthD:
+                insert_line(line, myday, airport, year)
+                myday +=1
+
+
+def grab_weather_data(top_airports, download, start_yr, end_yr, run_checks):
     """
     Obtain daily weather info for top 50 airports by looking up 
         ICAO codes on www.wunderground.com
     """
+    #pull weather data and save in CSV files on PC.  Do for each airport.
+    if download:
+        for ICAO_code in top_airports:
+            download_weather(ICAO_code, start_yr, end_yr)
 
-    #Access a months worth of tabular weather data in csv format from Weather
-        #Underground using the ICAO airport code. For instance, 
-        #to get the weather for San Francisco airport (ICAO code KSFO) 
-        #in the month of September 2013, go to:
-        #http://www.wunderground.com/history/airport/KSFO/2013/9/1/MonthlyHistory.html?format=1
+    #populate datatable with weather info
+    for ICAO_code in top_airports:
+        put_weather_in_table(ICAO_code, start_yr, end_yr)
+
+    if run_checks:
+        check_weatherD() #check what weatherD table contains        
+
+
+def corr_changes(top_airports, gap):
+    """For each pair of cities/airports determine how the daily change of 
+        #high temperature and cloud cover from one city predicts the daily change 
+        #of the other city 1,3, & 7 days in advance ###
+    """
+    #Note: Cloud cover from the Weather Underground tables ranges from 
+        #0 (clear) to 8 (completely cloudy)
+    
+    #I assume it is more efficient to use SQL's corr function rather than
+        #writing out a correlation formula(??)
+
     pass
-
 
 ##############################################################################
 
 
 def main():
 
+    #in SQLite, this opens up the database.  
+        #Just do this once  -- re-opening is costly and pointless
+    connection = sqlite3.connect("weather.db") 
+
     ### 1) Create a table of the 50 most travelled airports in the US ### 
-    get_airports("hw_6_data/ICAO_airports.csv", "hw_6_data/top_airports.csv")
+        #also, return list of 50 most travelled airports
+    top_airports = get_airports("hw_6_data/ICAO_airports.csv", "hw_6_data/top_airports.csv")
+    print "List of top 50 airports: " , top_airports
 
     ### 2) Build another table that will hold historical weather info ###
         #include min/max temperature, humidity, precipitation, and cloud cover
     build_weather_table(test=True)
 
     ### 3) Grab historical data from weather underground from 2008 until now ###
-        #populate your tables accordingly
-    grab_weather_data()
+        #populate table accordingly
+    grab_weather_data(top_airports[:2], False, 2012, 2013, True)
 
-    ### 4) For each pair of cities/airports determine how the daily change of 
-        #high temperature and cloud cover from one city predicts the daily change 
-        #of the other city 1,3, & 7 days in advance ###
-
+    ### 4) For each pair of cities/airports determine how the daily changes in
+        #one city predicts the daily change of the other city ###
+    corr_changes(top_airports[:2], gap=[(1,3,7)])
 
     ### 5) Plot the correlation strengths for the 10 top pairs ###
         #for all three dates, for both temperature and cloud cover 
